2d54cb41c29b743d2efdf6b295e35afcf2631e02
Ossi Herrala <oherrala@iki.fi>
2019-10-30 15:29:00 +0200

    allow Clippy nags from bindgen generated code

--- luomu-libpcap-sys/src/lib.rs
+++ luomu-libpcap-sys/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
+#![allow(clippy::unreadable_literal, clippy::redundant_static_lifetimes)]
 
 use libc::{sockaddr, timeval, FILE};
 
