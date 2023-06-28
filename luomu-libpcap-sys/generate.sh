289f85ae740a1670eb4354c71b2ed9c3ee6b66b1
Ossi Herrala <oherrala@iki.fi>
2023-06-28 20:37:06 +0300

    Regenerate libpcap bindings. Now with more traits implemented.

--- luomu-libpcap-sys/generate.sh
+++ luomu-libpcap-sys/generate.sh
@@ -11,30 +11,14 @@ gunzip -c "${LIBPCAP}.tar.gz" | tar xf - -C "${DEST}"
 
 bindgen \
     "${DEST}/${LIBPCAP}/pcap/pcap.h" \
-    --distrust-clang-mangling \
-    --use-core \
-    --ctypes-prefix='libc' \
+    -o src/pcap.rs \
     --allowlist-function='^pcap_.*' \
     --allowlist-type='^pcap_.*' \
     --allowlist-var='^PCAP_.*' \
-    --blocklist-type='^__.*' \
-    --blocklist-type='^sa_.*' \
-    --blocklist-type='^sockaddr' \
-    --blocklist-type='^size_t' \
-    --blocklist-type='^timeval' \
-    --blocklist-type='FILE' \
-    --blocklist-type='fpos_t' \
-    --blocklist-type='size_t' \
-    --blocklist-type='u_.*' \
-    -o src/pcap.rs \
-    -- \
-    -I"${DEST}/${LIBPCAP}"
-
-bindgen \
-    "${DEST}/${LIBPCAP}/pcap/dlt.h" \
-    --distrust-clang-mangling \
-    --use-core \
-    -o src/dlt.rs \
+    --allowlist-var='^DLT_.*' \
+    --opaque-type='^timeval' \
+    --opaque-type='^sockaddr' \
+    --opaque-type='^FILE' \
     -- \
     -I"${DEST}/${LIBPCAP}"
 
