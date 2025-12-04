sensorfu/luomu-libpcap

commit 61306eb412aeac67cef877e6fda9e178294d2a6e

Enable more Clippy lints and fix them

luomu-tpacketv3/src/ringbuf.rs
@@ -11,7 +11,8 @@ pub struct PacketDescriptor<'a> {
 impl<'a> PacketDescriptor<'a> {
     #[allow(clippy::cast_ptr_alignment)]
     pub fn get_next(self) -> PacketDescriptor<'a> {
-        let next_ptr = unsafe { self.ptr.offset(self.hdr.tp_next_offset as isize) };
+        let next_offset = isize::try_from(self.hdr.tp_next_offset).unwrap_or(isize::MAX);
+        let next_ptr = unsafe { self.ptr.offset(next_offset) };
         let next_hdr_ptr = next_ptr as *const if_packet::tpacket3_hdr;
         PacketDescriptor {
             ptr: next_ptr,
@@ -20,12 +21,14 @@ impl<'a> PacketDescriptor<'a> {
     }
 
     pub fn get_timestamp(&self) -> SystemTime {
-        UNIX_EPOCH + Duration::new(self.hdr.tp_sec as u64, self.hdr.tp_nsec)
+        UNIX_EPOCH + Duration::new(u64::from(self.hdr.tp_sec), self.hdr.tp_nsec)
     }
 
     pub fn get_packet_data(&self) -> &'a [u8] {
-        let data_ptr = unsafe { self.ptr.offset(self.hdr.tp_mac as isize) };
-        unsafe { std::slice::from_raw_parts(data_ptr, self.hdr.tp_snaplen as usize) }
+        let tp_mac = isize::try_from(self.hdr.tp_mac).unwrap_or(isize::MAX);
+        let data_ptr = unsafe { self.ptr.offset(tp_mac) };
+        let snaplen = usize::try_from(self.hdr.tp_snaplen).unwrap_or(usize::MAX);
+        unsafe { std::slice::from_raw_parts(data_ptr, snaplen) }
     }
 
     pub fn has_vlan_tci(&self) -> bool {
@@ -69,7 +72,7 @@ unsafe impl Send for BlockDescriptor<'_> {}
 
 impl<'a> BlockDescriptor<'a> {
     pub fn flush(&mut self) {
-        self.desc.hdr.block_status = if_packet::TP_STATUS_KERNEL
+        self.desc.hdr.block_status = if_packet::TP_STATUS_KERNEL;
     }
 
     pub fn is_ready(&self) -> bool {
@@ -81,14 +84,15 @@ impl<'a> BlockDescriptor<'a> {
     }
 
     pub fn get_first_packet(&self) -> PacketDescriptor<'a> {
-        unsafe { self.ptr.offset(self.desc.hdr.offset_to_first_pkt as isize) }.into()
+        let offset = isize::try_from(self.desc.hdr.offset_to_first_pkt).unwrap_or(isize::MAX);
+        unsafe { self.ptr.offset(offset) }.into()
     }
 }
 
 impl From<*mut u8> for BlockDescriptor<'_> {
     #[allow(clippy::cast_ptr_alignment)]
     fn from(ptr: *mut u8) -> Self {
-        let desc_ptr = ptr as *mut if_packet::tpacket_block_desc;
+        let desc_ptr = ptr.cast::<if_packet::tpacket_block_desc>();
 
         BlockDescriptor {
             ptr,
@@ -111,9 +115,11 @@ impl fmt::Display for BlockDescriptor<'_> {
 
 /// mmapped ringbuffer. Contains blocks of packet data, each block will be filled
 /// with packets by the kernel
+#[allow(clippy::struct_field_names)]
 pub struct Map {
     ptr: *mut libc::c_void,
     map_size: libc::size_t,
+    #[allow(dead_code)]
     block_size: u32,
     block_count: u32,
 }
@@ -124,7 +130,8 @@ impl Map {
     /// Create new ringbuffer.
     /// Buffer will contain given number of blocks of given size.
     pub fn create(block_size: u32, block_count: u32, fd: libc::c_int) -> Result<Map, std::io::Error> {
-        let size = (block_size * block_count) as libc::size_t;
+        let size = usize::try_from(block_size).unwrap_or(usize::MAX)
+            * usize::try_from(block_count).unwrap_or(usize::MAX);
         let p = unsafe {
             libc::mmap(
                 std::ptr::null_mut(),
@@ -148,15 +155,15 @@ impl Map {
 
     /// get pointer for descriptor block with given index.
     pub fn get_descriptor_ptr_for(&self, index: isize) -> *mut u8 {
-        if index >= self.block_count as isize {
-            panic!(
-                "Trying to read block {}, but only {} blocks available",
-                index, self.block_count
-            );
-        }
+        assert!(
+            index < isize::try_from(self.block_count).unwrap_or(isize::MAX),
+            "Trying to read block {index}, but only {} blocks available",
+            self.block_count
+        );
         let my_ptr = self.ptr;
-        let buf_ptr = my_ptr as *mut u8;
-        let offset = index * self.block_size as isize;
+        let buf_ptr = my_ptr.cast::<u8>();
+        let block_size = isize::try_from(self.block_count).unwrap_or(isize::MAX);
+        let offset = index * block_size;
         unsafe { buf_ptr.offset(offset) }
     }
 }

Cargo.toml
@@ -60,3 +60,21 @@ unsafe-code = "deny"
 
 # Allow cfg(kani)
 unexpected_cfgs = { level = "warn", check-cfg = ['cfg(kani)'] }
+
+[workspace.lints.clippy]
+# See https://doc.rust-lang.org/nightly/clippy/lints.html
+correctness = { level = "deny", priority = -1 }
+pedantic = { level = "warn", priority = -1 }
+perf = { level = "warn", priority = -1 }
+style = { level = "warn", priority = -1 }
+suspicious = { level = "warn", priority = -1 }
+
+## Allows
+# too early for this
+must_use_candidate = "allow"
+# Noisy lints
+doc_markdown = "allow"
+# Maybe once we start wrting proper docs, causes noise for now.
+missing_errors_doc = "allow"
+# Maybe need to be resolved one day
+cast_ptr_alignment = "allow"

luomu-common/src/addr_pair.rs
@@ -20,6 +20,7 @@ pub trait AddrPair<ADDR> {
     fn destination(&self) -> Destination<ADDR>;
 
     /// Return new address pair with source and destination addresses flipped.
+    #[must_use]
     fn flip(&self) -> Self;
 }
 
@@ -58,10 +59,7 @@ impl AddrPair<IpAddr> for IPPair {
         match (src.into_inner(), dst.into_inner()) {
             (IpAddr::V4(src), IpAddr::V4(dst)) => IPPair::new_v4(Source::new(src), Destination::new(dst)),
             (IpAddr::V6(src), IpAddr::V6(dst)) => IPPair::new_v6(Source::new(src), Destination::new(dst)),
-            _ => panic!(
-                "IPPair::new() invalid IP address families provided. src: {:?}, dst: {:?}",
-                src, dst
-            ),
+            _ => panic!("IPPair::new() invalid IP address families provided. src: {src:?}, dst: {dst:?}"),
         }
     }
 

luomu-common/src/address.rs
@@ -56,15 +56,15 @@ impl Address {
         match self {
             Address::Ipv4(ip) => Some(IpAddr::V4(*ip)),
             Address::Ipv6(ip) => Some(IpAddr::V6(*ip)),
-            _ => None,
+            Address::Mac(_) => None,
         }
     }
 
     /// Return the `MacAddr` or None
     pub const fn as_mac(&self) -> Option<MacAddr> {
         match self {
+            Address::Ipv4(_) | Address::Ipv6(_) => None,
             Address::Mac(mac) => Some(*mac),
-            _ => None,
         }
     }
 }
@@ -137,7 +137,7 @@ impl TryFrom<Address> for IpAddr {
         match addr {
             Address::Ipv4(ip) => Ok(ip.into()),
             Address::Ipv6(ip) => Ok(ip.into()),
-            _ => Err(InvalidAddress),
+            Address::Mac(_) => Err(InvalidAddress),
         }
     }
 }
@@ -148,7 +148,7 @@ impl TryFrom<&Address> for Ipv4Addr {
     fn try_from(addr: &Address) -> Result<Self, Self::Error> {
         match addr {
             Address::Ipv4(ip) => Ok(*ip),
-            _ => Err(InvalidAddress),
+            Address::Ipv6(_) | Address::Mac(_) => Err(InvalidAddress),
         }
     }
 }
@@ -159,7 +159,7 @@ impl TryFrom<&Address> for Ipv6Addr {
     fn try_from(addr: &Address) -> Result<Self, Self::Error> {
         match addr {
             Address::Ipv6(ip) => Ok(*ip),
-            _ => Err(InvalidAddress),
+            Address::Ipv4(_) | Address::Mac(_) => Err(InvalidAddress),
         }
     }
 }
@@ -171,7 +171,7 @@ impl TryFrom<&Address> for IpAddr {
         match addr {
             Address::Ipv4(ip) => Ok((*ip).into()),
             Address::Ipv6(ip) => Ok((*ip).into()),
-            _ => Err(InvalidAddress),
+            Address::Mac(_) => Err(InvalidAddress),
         }
     }
 }
@@ -182,7 +182,7 @@ impl TryFrom<Address> for MacAddr {
     fn try_from(addr: Address) -> Result<Self, Self::Error> {
         match addr {
             Address::Mac(mac) => Ok(mac),
-            _ => Err(InvalidAddress),
+            Address::Ipv4(_) | Address::Ipv6(_) => Err(InvalidAddress),
         }
     }
 }

luomu-common/src/macaddr.rs
@@ -6,10 +6,10 @@ use std::str::FromStr;
 use crate::{InvalidAddress, TagError};
 
 // The MAC address portion of u64
-const MAC_BITS: u64 = 0x0000FFFFFFFFFFFF;
+const MAC_BITS: u64 = 0x0000_FFFF_FFFF_FFFF;
 
 // The VLAN tag portion of u64
-const TAG_BITS: u64 = 0x0FFF000000000000;
+const TAG_BITS: u64 = 0x0FFF_0000_0000_0000;
 
 ///  Address bytes for Ethernet multicast MAC for All Nodes IPv6 multicast.
 const ALL_NODES_MAC: [u8; 6] = [0x33, 0x33, 0x00, 0x00, 0x00, 0x01];
@@ -53,6 +53,7 @@ impl MacAddr {
     pub const BROADCAST: MacAddr = MacAddr(MAC_BITS);
 
     /// Return MAC address as byte array in big endian order.
+    #[allow(clippy::missing_panics_doc)]
     pub fn as_array(&self) -> [u8; 6] {
         // Taking range of [2,7] is safe from u64. See kani proof in bunnies
         // module.
@@ -110,6 +111,7 @@ impl MacAddr {
 
     /// Peek a tag, but don't pop it out.
     pub const fn peek_tag(&self) -> Option<u16> {
+        #[allow(clippy::cast_possible_truncation)]
         match (self.0 >> 48) & 0x0FFF {
             n if n > 0 => Some(n as u16),
             _ => None,
@@ -239,7 +241,7 @@ impl FromStr for MacAddr {
     type Err = InvalidAddress;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.splitn(6, ":")
+        s.splitn(6, ':')
             .filter(|s| s.len() == 2)
             .filter(|s| s.chars().all(|c| c.is_ascii_hexdigit()))
             .map(|v| u8::from_str_radix(v, 16).ok())
@@ -268,7 +270,7 @@ impl fmt::Display for MacAddr {
         let h = self
             .as_array()
             .iter()
-            .map(|b| format!("{:02x}", b))
+            .map(|b| format!("{b:02x}"))
             .collect::<Box<[String]>>()
             .join(":");
         f.write_str(&h)

luomu-common/src/tagged_macaddr.rs
@@ -48,8 +48,9 @@ impl TagStack {
     }
 
     /// Peek a next tag in stack, but don't pop it out.
-    pub const fn peek_tag(&self) -> Option<u16> {
-        let tag = self.0 & 0x0000000000000FFF;
+    pub const fn peek_tag(self) -> Option<u16> {
+        #[allow(clippy::unusual_byte_groupings)] // groups of 12 bits
+        let tag = self.0 & 0x0000_000_000_000_FFF;
         if tag > 0 {
             return Some(tag as u16);
         }
@@ -59,7 +60,7 @@ impl TagStack {
 
     /// Get all tags as an array.
     #[allow(clippy::unusual_byte_groupings)]
-    pub fn tag_array(&self) -> [u16; 5] {
+    pub fn tag_array(self) -> [u16; 5] {
         let mut tags = [0u16; 5];
         tags[0] = ((self.0 & 0x0FFF_000_000_000_000) >> 48) as u16;
         tags[1] = ((self.0 & 0x0000_FFF_000_000_000) >> 36) as u16;

luomu-getifaddrs/src/lib.rs
@@ -123,18 +123,18 @@ const fn sa_get_family(sa: *const libc::sockaddr) -> i32 {
 
 /// Returns given [libc::sockaddr] pointer as a pointer to [libc::sockaddr_in]
 const fn sa_as_sockaddr_in(sa: *const libc::sockaddr) -> libc::sockaddr_in {
-    unsafe { *(sa as *const libc::sockaddr_in) }
+    unsafe { *(sa.cast::<libc::sockaddr_in>()) }
 }
 
 /// Returns given [libc::sockaddr] pointer as a pointer to [libc::sockaddr_in6]
 const fn sa_as_sockaddr_in6(sa: *const libc::sockaddr) -> libc::sockaddr_in6 {
-    unsafe { *(sa as *const libc::sockaddr_in6) }
+    unsafe { *(sa.cast::<libc::sockaddr_in6>()) }
 }
 
 #[cfg(target_os = "macos")]
 /// Returns given [libc::sockaddr] pointer as a pointer to [libc::sockaddr_dl]
 const fn sa_as_sockaddr_dl(sa: *const libc::sockaddr) -> libc::sockaddr_dl {
-    unsafe { *(sa as *const libc::sockaddr_dl) }
+    unsafe { *(sa.cast::<libc::sockaddr_dl>()) }
 }
 
 /// This struct provides access to information about network interface.
@@ -162,6 +162,8 @@ impl IfAddr {
     }
 
     /// Interface name
+    // Interface name comes from OS and should be sane (aka ASCII?)
+    #[allow(clippy::missing_panics_doc)]
     pub fn name(&self) -> &str {
         unsafe { CStr::from_ptr(self.ifa().ifa_name) }
             .to_str()
@@ -170,6 +172,7 @@ impl IfAddr {
 
     /// Interface flags
     pub const fn flags(&self) -> Flags {
+        #[allow(clippy::cast_possible_wrap)]
         Flags::from_bits_truncate(self.ifa().ifa_flags as i32)
     }
 
@@ -264,7 +267,7 @@ impl IfAddr {
                 // we need bytes for mac address, thus a bit of unsafery
                 let data = &a.sdl_data;
                 let sdl_data_as_u8: &[u8] =
-                    unsafe { slice::from_raw_parts(data.as_ptr() as *const u8, data.len()) };
+                    unsafe { slice::from_raw_parts(data.as_ptr().cast::<u8>(), data.len()) };
                 let mut address = [0u8; MAC_ADDR_LEN];
                 // mac address stored after name
                 // You may want to look into LLADDR() macro somewhere on Mac OS headers
@@ -275,7 +278,7 @@ impl IfAddr {
             #[cfg(target_os = "linux")]
             libc::AF_PACKET => {
                 // Mac address of the interface
-                let a: libc::sockaddr_ll = unsafe { *(ifa_addr as *const libc::sockaddr_ll) };
+                let a: libc::sockaddr_ll = unsafe { *(ifa_addr.cast::<libc::sockaddr_ll>()) };
                 let a_len = usize::from(a.sll_halen);
                 debug_assert!(a_len == MAC_ADDR_LEN);
                 if a_len != MAC_ADDR_LEN {

luomu-libpcap-sys/build.rs
@@ -15,10 +15,10 @@ fn main() -> io::Result<()> {
     let source_dir = unpack_libpcap(&out_dir)?;
     compile(&out_dir, &source_dir)?;
 
-    let libdir = format!("{}/lib", out_dir);
+    let libdir = format!("{out_dir}/lib");
 
     println!("cargo:rustc-link-lib=static=pcap");
-    println!("cargo:rustc-link-search=native={}", libdir);
+    println!("cargo:rustc-link-search=native={libdir}");
 
     Ok(())
 }
@@ -54,11 +54,10 @@ fn compile(out_dir: &str, source_dir: &Path) -> io::Result<()> {
 
     let compiler = cc::Build::new().get_compiler();
     let cc = compiler.path().to_string_lossy();
-    // panic!("cc = {}", cc);
 
     let output = Command::new(source_dir.join("configure"))
         .current_dir(source_dir)
-        .arg(format!("CC={}", cc))
+        .arg(format!("CC={cc}"))
         .arg("--prefix")
         .arg(out_dir)
         .arg("--disable-universal")
@@ -69,22 +68,22 @@ fn compile(out_dir: &str, source_dir: &Path) -> io::Result<()> {
         .arg(target_arg)
         .arg(host_arg)
         .output()?;
-    if !output.status.success() {
-        panic!(
-            "\nSTDOUT:\n{}\n\nSTDERR:\n{}\n",
-            String::from_utf8_lossy(&output.stdout),
-            String::from_utf8_lossy(&output.stderr)
-        );
-    }
+
+    assert!(
+        output.status.success(),
+        "\nSTDOUT:\n{}\n\nSTDERR:\n{}\n",
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr)
+    );
 
     let output = Command::new("make").current_dir(source_dir).arg(j_arg).output()?;
-    if !output.status.success() {
-        panic!(
-            "\nSTDOUT:\n{}\n\nSTDERR:\n{}\n",
-            String::from_utf8_lossy(&output.stdout),
-            String::from_utf8_lossy(&output.stderr)
-        );
-    }
+
+    assert!(
+        output.status.success(),
+        "\nSTDOUT:\n{}\n\nSTDERR:\n{}\n",
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr)
+    );
 
     Command::new("make")
         .current_dir(source_dir)

commit 7ceb4f90f80bb486820bf8daa4a50fbba431fb69

tpacketv3: Fix regression in block header offset calculation
A change in 61306eb412aeac67cef877e6fda9e178294d2a6e caused us to
invalidly use self.block_count instead of self.block_size when
calculating offset for block header.

This caused us to try to read block header data from invalid location
and likely try to access not allocated memory.

luomu-tpacketv3/src/ringbuf.rs
@@ -162,7 +162,7 @@ impl Map {
         );
         let my_ptr = self.ptr;
         let buf_ptr = my_ptr.cast::<u8>();
-        let block_size = isize::try_from(self.block_count).unwrap_or(isize::MAX);
+        let block_size = isize::try_from(self.block_size).unwrap_or(isize::MAX);
         let offset = index * block_size;
         unsafe { buf_ptr.offset(offset) }
     }