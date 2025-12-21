aebcea8f1e88a8ceb96d15991a58b7af05675fd2
Ossi Herrala <oherrala@iki.fi>
2025-12-22 01:28:03 +0200

    Add README for luomu-libpcap-sys

--- luomu-libpcap-sys/src/lib.rs
+++ luomu-libpcap-sys/src/lib.rs
@@ -1,3 +1,4 @@
+#![doc = include_str!("../README.md")]
 #![allow(missing_docs, unsafe_code)]
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 #![allow(clippy::unreadable_literal)]
